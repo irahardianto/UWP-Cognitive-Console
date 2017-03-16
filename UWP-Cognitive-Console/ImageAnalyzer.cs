dd5f4efa3e7a1d5e6c5f7b3b0e315c718f037d7b
irahardianto <ichsan.rahardianto@gmail.com>
2017-03-16 11:04:55 +0700

    Initial commit

---
+++ UWP-Cognitive-Console/ImageAnalyzer.cs
@@ -0,0 +1,305 @@
+﻿using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_Cognitive_Console
+{
+    public class ImageAnalyzer
+    {
+        private IEnumerable<Face> DetectedFaces { get; set; }
+        private IEnumerable<Emotion> DetectedEmotion { get; set; }
+
+        public List<Visitor> Visitors { get; set; }
+        public List<Person> CurrentPerson { get; set; }
+        public List<Guid> DetectedFaceIds { get; set; }
+        public List<Guid> CurrentFaceIds { get; set; }
+        public int? CurrentVisitorId { get; set; }
+        public int allowedPixelFrameDifference { get; set; }
+        public string FaceApiKey { get; set; }
+        public string EmotionApiKey { get; set; }
+
+        public ImageAnalyzer()
+        {
+            FaceApiKey = ""; 
+            EmotionApiKey = "";
+
+            DetectedFaceIds = new List<Guid>();
+            Visitors = new List<Visitor>();
+            allowedPixelFrameDifference = 5;
+        }
+
+        public async Task DetectFaces(byte[] imgBytes = null, string path = null)
+        {
+            CurrentFaceIds = new List<Guid>();
+
+            try
+            {
+                Stream imageFileStream = null;
+                if (!string.IsNullOrEmpty(path))
+                    imageFileStream = File.OpenRead(path);
+                else if (imgBytes != null)
+                    imageFileStream = new MemoryStream(imgBytes);
+
+                var requiredFaceAttributes = new FaceAttributeType[] {
+                        FaceAttributeType.Age,
+                        FaceAttributeType.Gender,
+                        FaceAttributeType.Smile,
+                        FaceAttributeType.FacialHair,
+                        FaceAttributeType.HeadPose,
+                        FaceAttributeType.Glasses
+                    };
+                var faceServiceClient = new FaceServiceClient(FaceApiKey);
+                var faces = await faceServiceClient.DetectAsync(
+                    imageFileStream, returnFaceId: true,
+                    returnFaceLandmarks: false,
+                    returnFaceAttributes: requiredFaceAttributes
+                    );
+
+                var zxc = faces;
+
+                foreach (var face in faces.ToArray())
+                {
+                    DetectedFaceIds.Add(face.FaceId);
+                    CurrentFaceIds.Add(face.FaceId);
+                }
+
+                this.DetectedFaces = faces.ToArray();
+            }
+            catch (Exception ex)
+            {
+                this.DetectedFaces = Enumerable.Empty<Face>();
+            }
+        }
+
+        public async Task DetectEmotion(byte[] imgBytes = null, string path = null)
+        {
+            try
+            {
+                Stream imageFileStream = null;
+                if (!string.IsNullOrEmpty(path))
+                    imageFileStream = File.OpenRead(path);
+                else if (imgBytes != null)
+                    imageFileStream = new MemoryStream(imgBytes);
+
+                EmotionServiceClient emotionServiceClient = new EmotionServiceClient(EmotionApiKey);
+
+                Emotion[] emotionResult = await emotionServiceClient.RecognizeAsync(imageFileStream);
+
+                var asd = emotionResult;
+
+                this.DetectedEmotion = emotionResult;
+            }
+            catch (Exception ex)
+            {
+                this.DetectedEmotion = Enumerable.Empty<Emotion>();
+            }
+        }
+
+        public async Task MapFaceIdEmotionResult()
+        {
+            CurrentPerson = new List<Person>();
+
+            foreach (var face in DetectedFaces)
+            {
+                CurrentPerson.Add(new Person
+                {
+                    VisitorId = Visitors.FirstOrDefault(x => x.FaceIds.Contains(face.FaceId)).Id,
+                    FaceId = face.FaceId,
+                    Age = (int)face.FaceAttributes.Age,
+                    Gender = face.FaceAttributes.Gender,
+                    Top = face.FaceRectangle.Top,
+                    Left = face.FaceRectangle.Left,
+                    Height = face.FaceRectangle.Height,
+                    Width = face.FaceRectangle.Width,
+                    HighestEmotion = SearchForMatchingEmotion(face)
+                });
+            }
+
+            foreach(var vis in Visitors)
+            {
+                if (vis.FaceIds.Any(v => CurrentPerson.Any(x => x.FaceId == v)))
+                {
+                    if(!vis.DwellingTime.IsRunning) { vis.DwellingTime.Start(); }
+                }
+            }
+        }
+
+        public async Task FindSimilarFace()
+        {
+            try
+            {
+                var faceIdsToCompare = new List<Guid>();
+                var faceServiceClient = new FaceServiceClient(FaceApiKey);
+
+                List<Guid> identifiedPeople = new List<Guid>();
+
+                if (Visitors.Count > 0)
+                {
+                    foreach (var vis in Visitors)
+                    {
+                        var cnt = vis.FaceIds.Count > 3 ? 3 : vis.FaceIds.Count;
+
+                        for (int i = 0; i < cnt; i++)
+                        {
+                            faceIdsToCompare.Add(vis.FaceIds[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    faceIdsToCompare = DetectedFaceIds;
+                }
+
+                foreach(var currentFaceId in CurrentFaceIds)
+                {
+                    var similarFace = await faceServiceClient.FindSimilarAsync(currentFaceId, faceIdsToCompare.ToArray(), 10);
+
+                    if (similarFace.Count() > 0)
+                    {
+                        foreach (var visitor in Visitors)
+                        {
+                            if (visitor.FaceIds.AsParallel().Any(x => similarFace.AsParallel().Any(y => y.FaceId == x)))
+                            {
+                                if (!visitor.FaceIds.Contains(currentFaceId))
+                                {
+                                    visitor.FaceIds.Add(currentFaceId);
+                                    visitor.LastSeen = DateTime.Now;
+                                }
+
+                                CurrentVisitorId = visitor.Id;
+                            }
+
+                            identifiedPeople.AddRange(visitor.FaceIds);
+                        }
+
+                        if (!identifiedPeople.AsParallel().Any(x => similarFace.AsParallel().Any(y => y.FaceId == x)))
+                        {
+                            var cnt = Visitors.Count;
+
+                            var newVisitor = new Visitor
+                            {
+                                Id = cnt + 1,
+                                FaceIds = new List<Guid> {
+                                    currentFaceId
+                                },
+                                LastSeen = DateTime.Now,
+                                DwellingTime = new Stopwatch()
+                            };
+
+                            CurrentVisitorId = newVisitor.Id;
+                            Visitors.Add(newVisitor);
+                        }
+                    }
+                    else
+                    {
+                        var cnt = Visitors.Count;
+
+                        var newVisitor = new Visitor
+                        {
+                            Id = cnt + 1,
+                            FaceIds = new List<Guid> {
+                                currentFaceId
+                            },
+                            LastSeen = DateTime.Now,
+                            DwellingTime = new Stopwatch()
+                        };
+
+                        CurrentVisitorId = newVisitor.Id;
+                        Visitors.Add(newVisitor);
+                    }
+                }
+            }
+            catch (FaceAPIException ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ErrorMessage);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public double GetDwellTime(int visitorId)
+        {
+            return Visitors.FirstOrDefault(x => x.Id == visitorId).DwellingTime.Elapsed.TotalSeconds;
+        }
+
+        public void CheckRunningDwellTime()
+        {
+            foreach (var vis in Visitors)
+            {
+                TimeSpan timeDifference = DateTime.Now - vis.LastSeen;
+                TimeSpan threeSeconds = TimeSpan.FromSeconds(3);
+
+                if (timeDifference > threeSeconds && vis.DwellingTime.IsRunning) { vis.DwellingTime.Stop(); vis.DwellingTime.Reset(); }
+            }
+        }
+
+        public string SearchForMatchingEmotion(Face currentFace)
+        {
+            var rectangleMatch = new RectangleMatch { LikelyScore = 0, HighestDetectedEmotion = "" };
+
+            foreach (var emo in DetectedEmotion)
+            {
+                var likelyMatchScore = 0;
+
+                likelyMatchScore = isMatchBetweenTolerance(emo.FaceRectangle.Top, currentFace.FaceRectangle.Top) ? likelyMatchScore + 1 : likelyMatchScore;
+                likelyMatchScore = isMatchBetweenTolerance(emo.FaceRectangle.Left, currentFace.FaceRectangle.Left) ? likelyMatchScore + 1 : likelyMatchScore;
+                likelyMatchScore = isMatchBetweenTolerance(emo.FaceRectangle.Height, currentFace.FaceRectangle.Height) ? likelyMatchScore + 1 : likelyMatchScore;
+                likelyMatchScore = isMatchBetweenTolerance(emo.FaceRectangle.Width, currentFace.FaceRectangle.Width) ? likelyMatchScore + 1 : likelyMatchScore;
+
+                if (rectangleMatch.LikelyScore < likelyMatchScore)
+                {
+                    var expr = emo.Scores.ToRankedList();
+                    var highestScore = expr.OrderByDescending(s => s.Value).First();
+                    var highestEmotion = highestScore.Key;
+
+                    rectangleMatch.LikelyScore = likelyMatchScore;
+                    rectangleMatch.HighestDetectedEmotion = highestEmotion;
+                }
+            }
+
+            return rectangleMatch.HighestDetectedEmotion;
+        }
+
+        public bool isMatchBetweenTolerance(int value1, int value2)
+        {
+            return Math.Abs(value1 - value2) <= allowedPixelFrameDifference ? true : false;
+        }
+
+        public class Visitor
+        {
+            public int Id { get; set; }
+            public List<Guid> FaceIds { get; set; }
+            public Stopwatch DwellingTime { get; set; }
+            public DateTime LastSeen { get; set; }
+        }
+
+        public class Person
+        {
+            public int VisitorId { get; set; }
+            public Guid FaceId { get; set; }
+            public int Age { get; set; }
+            public string Gender { get; set; }
+            public string HighestEmotion { get; set; }
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public int Top { get; set; }
+            public int Left { get; set; }
+        }
+
+        public class RectangleMatch
+        {
+            public int LikelyScore { get; set; }
+            public string HighestDetectedEmotion { get; set; }
+        }
+    }
+}
---
+++ UWP-Cognitive-Console/MainPage.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.Core;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_Cognitive_Console
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaCapture mediaCapture;
+        FaceDetectionEffect faceDetectionEffect;
+        public bool isDetectionProcessing { get; set; }
+        public bool isFaceDetected { get; set; }
+        public double CameraAspectRatio { get; set; }
+        public int CameraResolutionWidth { get; private set; }
+        public int CameraResolutionHeight { get; private set; }
+        public int? LastDetectedVisitorId { get; set; }
+
+        CoreDispatcher dispatcher = Windows.UI.Core.CoreWindow.GetForCurrentThread().Dispatcher;
+        ImageAnalyzer imageAnalyzer = new ImageAnalyzer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 360);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            isDetectionProcessing = false;
+            isFaceDetected = false;
+            
+            this.textBox.TextChanged += TextBox_TextChanged;
+
+            InitializeCamera();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += NetworkInterface.GetIsNetworkAvailable() == true ? "Internet connected successfully \n" : "Cannot connect to the internet \n";
+            textBox.Text += "Running detection loop \n";
+
+            while (true)
+            {
+                if (!isDetectionProcessing)
+                {
+                    try
+                    { 
+                        imageAnalyzer.CheckRunningDwellTime();
+
+                        await Task.WhenAll(DetectionLoop());
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                    }
+                }
+            }
+        }
+
+        public async void InitializeCamera()
+        {
+            textBox.Text += "Initializing Camera \n"; 
+
+            try
+            {
+                if (mediaCapture != null)
+                {
+                    mediaCapture.Dispose();
+                    mediaCapture = null;
+                }
+
+                mediaCapture = new MediaCapture();
+                await mediaCapture.InitializeAsync();
+
+                // Set callbacks for failure and recording limit exceeded
+                mediaCapture.Failed += new MediaCaptureFailedEventHandler(mediaCapture_Failed);
+                mediaCapture.RecordLimitationExceeded += new Windows.Media.Capture.RecordLimitationExceededEventHandler(mediaCapture_RecordLimitExceeded);
+
+                CaptureElement preview = new CaptureElement();
+                preview.Source = mediaCapture;
+                await mediaCapture.StartPreviewAsync();
+
+                SetVideoEncodingToHighestResolution();
+
+                if (faceDetectionEffect == null || !faceDetectionEffect.Enabled)
+                {
+                    await CreateFaceDetectionEffectAsync();
+                }
+                else
+                {
+                    //await CleanUpFaceDetectionEffectAsync();
+                }
+
+                textBox.Text += "Initializing Camera Success \n";
+            }
+            catch (Exception ex)
+            {
+                textBox.Text += $"Initializing Camera Failed { ex.Message } \n";
+            }
+        }
+
+        public async Task DetectionLoop()
+        {
+            Debug.WriteLine("Running Detection Loop");
+            isDetectionProcessing = true;
+            var startTime = DateTime.Now;
+
+            var photoFile = await DownloadsFolder.CreateFileAsync("tmp", CreationCollisionOption.GenerateUniqueName);
+            ImageEncodingProperties imageProperties = ImageEncodingProperties.CreateJpeg();
+
+            await mediaCapture.CapturePhotoToStorageFileAsync(imageProperties, photoFile);
+            IRandomAccessStream stream = await photoFile.OpenReadAsync();
+            var imageStream = ReadFully(stream.AsStream());
+
+            if (isFaceDetected)
+            {
+                isFaceDetected = false;
+
+                Debug.WriteLine("Processing Detection");
+
+                await Task.WhenAll(imageAnalyzer.DetectFaces(imageStream), imageAnalyzer.DetectEmotion(imageStream));
+                await Task.WhenAll(imageAnalyzer.FindSimilarFace());
+                await Task.WhenAll(imageAnalyzer.MapFaceIdEmotionResult());
+
+                TimeSpan timeDifference = DateTime.Now - startTime;
+
+                await dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+
+                    foreach (var person in imageAnalyzer.CurrentPerson)
+                    {
+                        var faceId = person.VisitorId;
+                        var age = person.Age.ToString();
+                        var gender = person.Gender;
+                        var emotion = person.HighestEmotion;
+                        var dwell = imageAnalyzer.GetDwellTime(person.VisitorId);
+
+                        textBox.Text += $"[ { DateTime.Now } ] - {((int)timeDifference.Milliseconds).ToString()}ms - face id : { faceId } | age : { age } | sex : { gender } | emotion : { emotion } | dwell : { dwell } \n";
+                    }
+                });
+            }
+            isDetectionProcessing = false;
+        }
+
+        public byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.scrollViewer.ChangeView(0.0f, this.scrollViewer.ExtentHeight, 1.0f);
+        }
+
+        private async Task CreateFaceDetectionEffectAsync()
+        {
+            // Create the definition, which will contain some initialization settings
+            var definition = new FaceDetectionEffectDefinition();
+
+            // To ensure preview smoothness, do not delay incoming samples
+            definition.SynchronousDetectionEnabled = false;
+
+            // In this scenario, choose detection speed over accuracy
+            definition.DetectionMode = FaceDetectionMode.HighPerformance;
+
+            // Add the effect to the preview stream
+            faceDetectionEffect = (FaceDetectionEffect)await mediaCapture.AddVideoEffectAsync(definition, MediaStreamType.VideoPreview);
+
+            // Register for face detection events
+            faceDetectionEffect.FaceDetected += FaceDetectionEffect_FaceDetected;
+
+            // Choose the shortest interval between detection events
+            faceDetectionEffect.DesiredDetectionInterval = TimeSpan.FromMilliseconds(33);
+
+            // Start detecting faces
+            faceDetectionEffect.Enabled = true;
+        }
+
+        private async void FaceDetectionEffect_FaceDetected(FaceDetectionEffect sender, FaceDetectedEventArgs args)
+        {
+            isFaceDetected = args.ResultFrame.DetectedFaces.Count > 0 ? true : false;
+        }
+
+        private async void SetVideoEncodingToHighestResolution()
+        {
+            VideoEncodingProperties highestVideoEncodingSetting;
+            uint maxHeightForRealTime = 720;
+            highestVideoEncodingSetting = mediaCapture.VideoDeviceController.GetAvailableMediaStreamProperties(MediaStreamType.VideoPreview).Cast<VideoEncodingProperties>().Where(v => v.Height <= maxHeightForRealTime).OrderByDescending(v => v.Width * v.Height * (v.FrameRate.Numerator / v.FrameRate.Denominator)).First();
+
+            if (highestVideoEncodingSetting != null)
+            {
+                this.CameraAspectRatio = (double)highestVideoEncodingSetting.Width / (double)highestVideoEncodingSetting.Height;
+                this.CameraResolutionHeight = (int)highestVideoEncodingSetting.Height;
+                this.CameraResolutionWidth = (int)highestVideoEncodingSetting.Width;
+
+                await mediaCapture.VideoDeviceController.SetMediaStreamPropertiesAsync(MediaStreamType.VideoPreview, highestVideoEncodingSetting);
+            }
+        }
+
+        private void mediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void mediaCapture_RecordLimitExceeded(MediaCapture sender)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
